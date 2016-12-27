e9e1d887e1998d94af81e2145d23520ed5306d70
Will Tinsdeall <will.tinsdeall@mercianlabels.com>
2016-12-27 19:37:49 +0000

    Create count_pf_mail.pl

---
+++ zimbra/zimbra-mta/count_pf_mail.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/perl
+use File::ReadBackwards;
+use POSIX "strftime";
+
+my $items;
+my $time = $ARGV[0];
+my $logfile = "/var/log/mail.log";
+my $search_string = "status=sent";
+my $goback = POSIX::strftime( "%b %d %H:%M:%S", localtime( time() - $time * 60 )                                                                                         );
+
+$readback = File::ReadBackwards->new( $logfile ) or
+                        die "unable to open '$logfile' $!" ;
+
+while ( defined( my $line = $readback->readline() ) ) {
+
+    last if $line lt $goback;
+
+    if ($line =~ /$search_string/) {
+        print $line;
+    }
+
+}
